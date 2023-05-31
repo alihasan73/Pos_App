ffe1d9f637d069849dd69040ca74f65e8ecd1400
Iyalodon <rijalman.sr@gmail.com>
2023-05-31 10:26:04 +0700

    sdfsdfsd
    sdfsfsdf

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -22,14 +22,14 @@
 //   }
 // }
 const { db_username, db_password, db_database, db_host, db_dialect } =
-	process.env;
+  process.env;
 
 module.exports = {
-	development: {
-		username: db_username,
-		password: db_password,
-		database: db_database,
-		host: db_host,
-		dialect: db_dialect,
-	},
+  development: {
+    username: db_username,
+    password: db_password,
+    database: db_database,
+    host: db_host,
+    dialect: db_dialect,
+  },
 };
