0d6bf48df25346f28b1783745daeff02afd45ee1
fahminurk <fahminurk31@gmail.com>
2023-06-06 09:54:23 +0700

    backend

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers").userController;
-
+const { fileUploader } = require("../middlewares/multer");
 //register cashier
 router.post("/registerCashier", userController.registerCashier);
 
 //REGISTER ADMIN
-router.post("/registerAdmin", userController.registerAdmin);
+router.post(
+  "/registerAdmin",
+  fileUploader({
+    destinationFolder: "avatar",
+  }).single("avatar"),
+  userController.registerAdmin
+);
 
-//login cashier
-// router.post("/loginCashier", userController.loginCashier);
-
-//login admin
-// router.post("/loginAdmin", userController.loginAdmin);
+//get all
+router.get("/", userController.getAllUser);
 
 //login
 router.post("/login", userController.login);
