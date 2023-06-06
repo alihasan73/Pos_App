d0e9d84ba8aaab64bcf7023721e706e4fe407696
Hasan Ali <hasansanad73@gmail.com>
2023-06-06 10:00:42 +0700

    tes

--- server/src/models/produk.js
+++ server/src/models/produk.js
@@ -3,7 +3,7 @@ module.exports = (sequelize, Sequelize) => {
 		name: Sequelize.STRING,
 		description: Sequelize.STRING,
 		price: Sequelize.INTEGER,
-		// category_id: Sequelize.INTEGER,
+		category_id: Sequelize.INTEGER,
 		product_url: Sequelize.STRING,
 	});
 	return Produk;
