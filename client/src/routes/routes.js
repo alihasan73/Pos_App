06242a4f62f3c99806dceaa25fbebbbe39227f92
fahminurk <fahminurk31@gmail.com>
2023-06-12 19:19:18 +0700

    new

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -4,90 +4,80 @@ import LoginPage from "../pages/loginPage";
 import ProductPage from "../pages/ProductPage";
 import DashboardAdminPage from "../pages/dashboardAdminPage";
 import CategoryPage from "../pages/categoryPage";
-import DashbordCasher from "../pages/dashboardCasherPage";
 import AdminSettings from "../pages/adminSettings";
 import DashboardCashier from "../pages/dashboardCashier";
 import ReportPage from "../pages/reportPage";
 import Test from "../pages/categoryPage";
-import CasherHistory from "../pages/casherHistory";
 
 const routes = [
-	<Route
-		path="/login"
-		key={"loginPage"}
-		element={
-			<ProtectedPage guestOnly={true}>
-				<LoginPage />
-			</ProtectedPage>
-		}
-	/>,
+  <Route
+    path="/login"
+    key={"loginPage"}
+    element={
+      <ProtectedPage guestOnly={true}>
+        <LoginPage />
+      </ProtectedPage>
+    }
+  />,
 
-	<Route
-		path="dashboardCashier"
-		element={
-			<ProtectedPage cashierOnly={true}>
-				<DashboardCashier />
-			</ProtectedPage>
-		}
-	/>,
-	<Route
-		path="/historyCashier"
-		element={
-			<ProtectedPage cashierOnly={true}>
-				<CasherHistory />
-			</ProtectedPage>
-		}
-	/>,
+  <Route
+    path="dashboardCashier"
+    element={
+      <ProtectedPage cashierOnly={true}>
+        <DashboardCashier />
+      </ProtectedPage>
+    }
+  />,
 
-	<Route
-		path="/dashboardAdmin"
-		element={
-			<ProtectedPage adminOnly={true}>
-				<DashboardAdminPage />
-			</ProtectedPage>
-		}
-	/>,
-	<Route
-		path="/products"
-		element={
-			<ProtectedPage adminOnly={true}>
-				<ProductPage />
-			</ProtectedPage>
-		}
-	/>,
-	<Route
-		path="/categories"
-		element={
-			<ProtectedPage adminOnly={true}>
-				<CategoryPage />
-				{/* <Test /> */}
-			</ProtectedPage>
-		}
-	/>,
+  <Route
+    path="/dashboardAdmin"
+    element={
+      <ProtectedPage adminOnly={true}>
+        <DashboardAdminPage />
+      </ProtectedPage>
+    }
+  />,
+  <Route
+    path="/products"
+    element={
+      <ProtectedPage adminOnly={true}>
+        <ProductPage />
+      </ProtectedPage>
+    }
+  />,
+  <Route
+    path="/categories"
+    element={
+      <ProtectedPage adminOnly={true}>
+        <CategoryPage />
+        {/* <Test /> */}
+      </ProtectedPage>
+    }
+  />,
 
-	<Route
-		path="/adminSettings"
-		element={
-			<ProtectedPage adminOnly={true}>
-				<AdminSettings />
-			</ProtectedPage>
-		}
-	/>,
-	<Route
-		path="/report"
-		element={
-			<ProtectedPage adminOnly={true}>
-				<ReportPage />
-			</ProtectedPage>
-		}
-	/>,
+  <Route
+    path="/adminSettings"
+    element={
+      <ProtectedPage adminOnly={true}>
+        <AdminSettings />
+      </ProtectedPage>
+    }
+  />,
+  <Route
+    path="/report"
+    element={
+      <ProtectedPage adminOnly={true}>
+        <ReportPage />
+      </ProtectedPage>
+    }
+  />,
 
-	<Route
-		path="/*"
-		element={
-			<ProtectedPage guestOnly={true} adminOnly={true} cashierOnly={true} />
-		}
-	/>,
+  <Route
+    path="/*"
+    element={
+      <ProtectedPage guestOnly={true} adminOnly={true} cashierOnly={true} />
+    }
+  />,
 ];
 
 export default routes;
