83949327ba7e9e93ccc85b30996c2809043f441c
Hasan Ali <hasansanad73@gmail.com>
2023-06-12 09:46:06 +0700

    route

--- client/src/routes/protectedPage.js
+++ client/src/routes/protectedPage.js
@@ -7,7 +7,6 @@ export default function ProtectedPage({
 	adminOnly = false,
 	cashierOnly = false,
 }) {
-<<<<<<< HEAD
 	const userSelector = useSelector((state) => state.auth);
 	const nav = useNavigate();
 	const token = JSON.parse(localStorage.getItem("user"));
@@ -27,25 +26,4 @@ export default function ProtectedPage({
 	}, [userSelector, adminOnly, cashierOnly]);
 
 	return children;
-=======
-  const userSelector = useSelector((state) => state.auth);
-  const nav = useNavigate();
-  const token = JSON.parse(localStorage.getItem("user"));
-  // console.log(adminOnly);
-  console.log(userSelector);
-  console.log(token);
-  useEffect(() => {
-    if (adminOnly && token && userSelector.role == "ADMIN") {
-      //jika admin dan login ke dashboard admin
-      return nav("/dashboardAdmin");
-    } else if (cashierOnly && userSelector.role == "CASHIER") {
-      //jika  cashier dan login maka ke dashboard cashier
-      return nav("/dashboardCashier");
-    } else {
-      return nav("/login");
-    }
-  }, [userSelector, adminOnly, cashierOnly]);
-
-  return children;
->>>>>>> 8fee927a57769c9f970eaa0101a26b59553efce2
 }
--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -5,7 +5,6 @@ import TestPage from "../pages/testPage";
 import ProductPage from "../pages/ProductPage";
 import DashboardAdminPage from "../pages/dashboardAdminPage";
 import CategoryPage from "../pages/categoryPage";
-<<<<<<< HEAD
 import DashbordCasher from "../pages/dashboardCasherPage";
 import AdminSettings from "../pages/adminSettings";
 import CasherHistory from "../pages/casherHistory";
@@ -59,57 +58,6 @@ const routes = [
 	<Route path="/test" element={<TestPage />} />,
 	<Route path="/casher" element={<DashbordCasher />} />,
 	<Route path="/casherHistory" element={<CasherHistory />} />,
-=======
-import AdminSettings from "../pages/adminSettings";
-
-const routes = [
-  <Route
-    path="/login"
-    key={"loginPage"}
-    element={
-      <ProtectedPage>
-        <LoginPage />
-      </ProtectedPage>
-    }
-  />,
-
-  <Route path="cashier" />,
-  <Route
-    path="/dashboardAdmin"
-    element={
-      <ProtectedPage adminOnly={true}>
-        <DashboardAdminPage />
-      </ProtectedPage>
-    }
-  />,
-  <Route
-    path="/products"
-    element={
-      <ProtectedPage adminOnly={true}>
-        <ProductPage />
-      </ProtectedPage>
-    }
-  />,
-  <Route
-    path="/categories"
-    element={
-      <ProtectedPage adminOnly={true}>
-        <CategoryPage />
-      </ProtectedPage>
-    }
-  />,
-
-  <Route
-    path="/adminSettings"
-    element={
-      <ProtectedPage adminOnly={true}>
-        <AdminSettings />
-      </ProtectedPage>
-    }
-  />,
-
-  <Route path="/test" element={<TestPage />} />,
->>>>>>> 8fee927a57769c9f970eaa0101a26b59553efce2
 ];
 
 export default routes;
